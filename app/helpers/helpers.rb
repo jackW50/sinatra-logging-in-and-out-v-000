029a2795cb5fd8d312fb9c1530e6659a91859261
John Werenski <jtwerenski@gmail.com>
2018-12-19 17:45:45 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   
   def self.current_user(session)
-    User.find(session[:id])
+    User.find(session[:user_id])
   end 
   
   def self.is_logged_in?(session)
-    !!session[:id] ? true : false  
+    !!session[:user_id] ? true : false  
   end 
   
 end
